4aec16ccbe7ad8baeffda8006fcea6061d8bbb1e
MEYLYMENDOZA <22200248@ue.edu.pe>
2025-11-24 10:27:41 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto01.API/Program.cs
@@ -0,0 +1,34 @@
+﻿using Proyecto01.API; // Para tu Contexto
+using Microsoft.EntityFrameworkCore; // Para el método UseSqlServer
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var _config = builder.Configuration;
+var cnx = _config.GetConnectionString("DefaultConnection");
+
+// ✅ Línea de configuración CORREGIDA para Entity Framework Core
+builder.Services.AddDbContext<Proyecto01Context>(options =>
+    options.UseSqlServer(cnx));
+// -------------------------------------------------------------
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
